48c5c5adbed16dc61849c057f0123f85591c0b6a
jackytsu <jackytsu@vip.qq.com>
2020-07-20 20:53:23 +0800

    将项目重构为TS项目

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,6 +1,9 @@
 # Change Log
 All notable changes to the "Text Transformer for VS Code" extension will be documented in this file.
 
+## [0.0.4]
+- turn the project to type script.
+
 ## [0.0.3]
 - update package.json info.
 - upgrade the dependencies.
--- src/text-transformer.ts
+++ src/text-transformer.ts
@@ -1,9 +1,10 @@
-const vscode = require("vscode");
+import * as vscode from "vscode";
 
-function activate(context) {
-
-    const getStringArray = function (text) {
-        let str = [], lastPos = 0, i;
+function activate(context: vscode.ExtensionContext) {
+    const getStringArray = function (text: string) {
+        const str = [];
+        let lastPos = 0,
+            i;
 
         for (i = 0; i < text.length; i++) {
             if (/[\s\-_A-Z]/.test(text.charAt(i))) {
@@ -25,20 +26,20 @@ function activate(context) {
         return str;
     };
 
-    const textTransform = function (separator) {
-        let editor = vscode.window.activeTextEditor;
+    const textTransform = function (separator: string) {
+        const editor = vscode.window.activeTextEditor;
         if (editor) {
             if (!editor.selections || editor.selections.length === 0) {
-                vscode.window.showInformationMessage('There is no selected text!');
+                vscode.window.showInformationMessage("There is no selected text!");
                 return;
             }
 
             editor.edit(function (edit) {
                 for (let i = 0; i < editor.selections.length; i++) {
-                    let selection = editor.selections[i];
+                    const selection = editor.selections[i];
 
                     if (!selection.isEmpty) {
-                        let range = new vscode.Range(selection.start, selection.end);
+                        const range = new vscode.Range(selection.start, selection.end);
                         let text = editor.document.getText(range);
 
                         text = getStringArray(text).join(separator);
@@ -47,86 +48,86 @@ function activate(context) {
                 }
             });
         } else {
-            vscode.window.showInformationMessage('There is no activeTextEditor!');
+            vscode.window.showInformationMessage("There is no activeTextEditor!");
         }
     };
 
     // console.log('Congratulations, your extension "text-transformer" is now active!');
 
-    let dashed = vscode.commands.registerCommand('text-transformer.dashed', function () {
-        textTransform('-');
+    const dashed = vscode.commands.registerCommand("text-transformer.dashed", function () {
+        textTransform("-");
     });
     context.subscriptions.push(dashed);
 
-    let underline = vscode.commands.registerCommand('text-transformer.underline', function () {
-        textTransform('_');
+    const underline = vscode.commands.registerCommand("text-transformer.underline", function () {
+        textTransform("_");
     });
     context.subscriptions.push(underline);
 
-    let upper = vscode.commands.registerCommand('text-transformer.upper', function () {
-        let editor = vscode.window.activeTextEditor;
+    const upper = vscode.commands.registerCommand("text-transformer.upper", function () {
+        const editor = vscode.window.activeTextEditor;
         if (editor) {
             if (!editor.selections || editor.selections.length === 0) {
-                vscode.window.showInformationMessage('There is no selected text!');
+                vscode.window.showInformationMessage("There is no selected text!");
                 return;
             }
 
             editor.edit(function (edit) {
                 for (let i = 0; i < editor.selections.length; i++) {
-                    let selection = editor.selections[i];
+                    const selection = editor.selections[i];
 
                     if (!selection.isEmpty) {
-                        let range = new vscode.Range(selection.start, selection.end);
+                        const range = new vscode.Range(selection.start, selection.end);
                         const text = editor.document.getText(range).toUpperCase();
                         edit.replace(range, text);
                     }
                 }
             });
         } else {
-            vscode.window.showInformationMessage('There is no activeTextEditor!');
+            vscode.window.showInformationMessage("There is no activeTextEditor!");
         }
     });
     context.subscriptions.push(upper);
 
-    let lower = vscode.commands.registerCommand('text-transformer.lower', function () {
-        let editor = vscode.window.activeTextEditor;
+    const lower = vscode.commands.registerCommand("text-transformer.lower", function () {
+        const editor = vscode.window.activeTextEditor;
         if (editor) {
             if (!editor.selections || editor.selections.length === 0) {
-                vscode.window.showInformationMessage('There is no selected text!');
+                vscode.window.showInformationMessage("There is no selected text!");
                 return;
             }
 
             editor.edit(function (edit) {
                 for (let i = 0; i < editor.selections.length; i++) {
-                    let selection = editor.selections[i];
+                    const selection = editor.selections[i];
 
                     if (!selection.isEmpty) {
-                        let range = new vscode.Range(selection.start, selection.end);
+                        const range = new vscode.Range(selection.start, selection.end);
                         const text = editor.document.getText(range).toLowerCase();
                         edit.replace(range, text);
                     }
                 }
             });
         } else {
-            vscode.window.showInformationMessage('There is no activeTextEditor!');
+            vscode.window.showInformationMessage("There is no activeTextEditor!");
         }
     });
     context.subscriptions.push(lower);
 
-    let reverse = vscode.commands.registerCommand('text-transformer.reverse', function () {
-        let editor = vscode.window.activeTextEditor;
+    const reverse = vscode.commands.registerCommand("text-transformer.reverse", function () {
+        const editor = vscode.window.activeTextEditor;
         if (editor) {
             if (!editor.selections || editor.selections.length === 0) {
-                vscode.window.showInformationMessage('There is no selected text!');
+                vscode.window.showInformationMessage("There is no selected text!");
                 return;
             }
 
             editor.edit(function (edit) {
                 for (let i = 0; i < editor.selections.length; i++) {
-                    let selection = editor.selections[i];
+                    const selection = editor.selections[i];
 
                     if (!selection.isEmpty) {
-                        let range = new vscode.Range(selection.start, selection.end);
+                        const range = new vscode.Range(selection.start, selection.end);
                         const text = editor.document.getText(range);
                         const str = [];
 
@@ -134,31 +135,31 @@ function activate(context) {
                             const s = text.charAt(i);
                             str.push(s.toLowerCase() === s ? s.toUpperCase() : s.toLowerCase());
                         }
-                        edit.replace(range, str.join(''));
+                        edit.replace(range, str.join(""));
                     }
                 }
             });
         } else {
-            vscode.window.showInformationMessage('There is no activeTextEditor!');
+            vscode.window.showInformationMessage("There is no activeTextEditor!");
         }
     });
     context.subscriptions.push(reverse);
 
-    let camel = vscode.commands.registerCommand('text-transformer.camel', function () {
-        let editor = vscode.window.activeTextEditor;
+    const camel = vscode.commands.registerCommand("text-transformer.camel", function () {
+        const editor = vscode.window.activeTextEditor;
         if (editor) {
             if (!editor.selections || editor.selections.length === 0) {
-                vscode.window.showInformationMessage('There is no selected text!');
+                vscode.window.showInformationMessage("There is no selected text!");
                 return;
             }
 
             editor.edit(function (edit) {
                 for (let i = 0; i < editor.selections.length; i++) {
-                    let selection = editor.selections[i];
+                    const selection = editor.selections[i];
 
                     if (!selection.isEmpty) {
-                        let range = new vscode.Range(selection.start, selection.end);
-                        let text = getStringArray(editor.document.getText(range)).join('-');
+                        const range = new vscode.Range(selection.start, selection.end);
+                        let text = getStringArray(editor.document.getText(range)).join("-");
                         text = text.replace(/(-[a-z])/g, function (w) {
                             return w.toUpperCase().replace(/-/, "");
                         });
@@ -167,16 +168,12 @@ function activate(context) {
                 }
             });
         } else {
-            vscode.window.showInformationMessage('There is no activeTextEditor!');
+            vscode.window.showInformationMessage("There is no activeTextEditor!");
         }
     });
     context.subscriptions.push(camel);
 }
 
-function deactivate() {
-}
-
 module.exports = {
     activate,
-    deactivate
 };
